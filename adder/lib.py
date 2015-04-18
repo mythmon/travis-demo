e268d94858c8e1b17c8759d6934457fdbdfeaa22
Mike Cooper <mythmon@gmail.com>
2015-04-18 11:50:03 -0700

    Initial commit.

---
+++ adder/lib.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+def add(a, b):
+    return a + b
+
+
+class TestAdd(unittest.TestCase):
+
+    def test_it_works(self):
+        assert add(1, 2) == 3
+
+    def test_negative(self):
+        assert add(1, -2) == -1
+
